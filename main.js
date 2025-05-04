07ba270a7aa1f8ca3fcc1c530a293074d9c0f2fc
aprar-jalal <s12324682@stu.najah.edu>
2025-05-04 06:47:57 -0700

    tasks

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 
-var showPasswordBtn= document.getElementById("showPasswordBtn");
+var monthes=" ";
+
+for(var i =1 ; i<=12;i++){
+    monthes+=`<option>${i}</option>`
+}
+
+document.getElementById("months").innerHTML=monthes;
 
 
-showPasswordBtn.onclick=ShowPassword;
-function ShowPassword(){
-    document.getElementById("password").setAttribute('type','text');
-};
 
+var user={
+    name:'tariq',
+    age:31,
+    email:'tariq@gmail.com',
+    city:'qalqilia'
+}
 
-
-
+document.getElementById("name").textContent=user.name;
+document.getElementById("age").textContent=user.age;
+document.getElementById("email").textContent=user.email;
+document.getElementById("city").textContent=user.city;
